d1253296d0dc20f4e7560b928c55464701c3ba76
Hector Castillo <hectordanielunsc@gmail.com>
2017-07-03 15:24:31 -0500

    divición en funciones de api nodo crunchyroll GET y creación getIds

--- api/nodes/crunchyroll/methods/GET.php
+++ api/nodes/crunchyroll/methods/GET.php
@@ -1,11 +1,12 @@
 <?php
 
-//responder ids para el proxy
-
-
 //abrir SQL
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 
+//function para obtener todas las ids disponibles
+function getIds(){
+    /* Esta funcion obtiene todas las ids con pass=0, se usara para que el proxy
+      espere el subtitulo en espanil y posteriormente subira los datos y acutalizara el pass=1  */
 
     //consultar si existe la ID enviada
     $prep_stmt = "SELECT a.id, a.idEpisodio, a.idCrunchy, a.subRequerido, a.subtitleId, a.streamInfo, a.pass, c.title, c.id FROM crunchyroll as a, episodios as b, animes as c WHERE a.pass = 0 AND a.idEpisodio = b.id AND b.parentId = c.id AND c.status = 1 ORDER BY RAND() LIMIT 50;";
@@ -37,3 +38,12 @@ $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 
 
 respuesta_ok( array( "items" => $responseData, "count" => count($responseData) ), 200);
+}
+// fin de la function getIds
+
+
+
+//condiciones para responder segun la petición
+          if($value == "pending"){
+            getIds();
+          }
