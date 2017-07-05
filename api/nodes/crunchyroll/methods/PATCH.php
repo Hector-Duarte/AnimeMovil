5046de374189861d5fee2d8401a1e274f32fabff
Hector Castillo <hectordanielunsc@gmail.com>
2017-07-05 14:40:34 -0500

    prueba #1 con el metodo patch crunchyroll api

--- api/nodes/crunchyroll/methods/PATCH.php
+++ api/nodes/crunchyroll/methods/PATCH.php
@@ -10,8 +10,19 @@ if( !is_numeric($_GET['value']) ){
   error("El ID proporcionado no es valido.", 400);
 }
 
-//consultar si existe la ID enviada
-$prep_stmt = "UPDATE crunchyroll SET  WHERE idCrunchy = ? LIMIT 1;";
+//entrada de datos
+$input = json_decode(file_get_contents('php://input'));
+
+
+
+//preparar valores a actualizar
+$query_set = " pass = $input->pass ";
+
+echo $query_set;exit();
+
+
+//actualizar el elemento
+$prep_stmt = "UPDATE crunchyroll SET  WHERE id = ? LIMIT 1;";
 $stmt = $mysqli->prepare($prep_stmt);
 
 $stmt->bind_param('i', $idCrunchy);
