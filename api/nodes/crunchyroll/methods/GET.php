660e53a470ea0d968e90947436c5107343b93e67
Hector Castillo <hectordanielunsc@gmail.com>
2017-07-06 20:58:08 -0500

    Agregado 'pendingEncoding' para recojer datos que tengan pass = 1

--- api/nodes/crunchyroll/methods/GET.php
+++ api/nodes/crunchyroll/methods/GET.php
@@ -41,6 +41,49 @@ if($_GET['value'] == 'pending'){
   }//fin de if pending
 
 
+
+
+  //function para obtener todas las ids disponibles
+  if($_GET['value'] == 'pendingEncoding'){
+      /* Esta funcion obtiene todas las ids con pass=0, se usara para que el proxy
+        espere el subtitulo en espanil y posteriormente subira los datos y acutalizara el pass=1  */
+
+      //consultar todas las ids disponibles
+      $prep_stmt = "SELECT a.id, a.idEpisodio, a.idCrunchy, a.subRequerido, a.subtitleId, a.streamInfo, a.pass, c.title, c.id FROM crunchyroll as a, episodios as b, animes as c WHERE a.pass = 1 AND a.idEpisodio = b.id AND b.parentId = c.id AND c.status = 1 ORDER BY RAND() LIMIT 50;";
+      $stmt = $mysqli->prepare($prep_stmt);
+
+      $stmt->execute();
+      $stmt->store_result();
+
+      //asignar valores recibidos
+      $stmt->bind_result($id, $idEpisodio, $idCrunchy, $subRequerido, $subtitleId, $streamInfo, $pass, $anime_title, $anime_id);
+
+      //array contenedor
+      $responseData = array();
+
+      //ciclo para imprimir valores
+      while( $stmt->fetch() ){
+
+        $responseData[] = array( "id" => $id,
+                                 "idEpisodio" => $idEpisodio,
+                                 "idCrunchy" => $idCrunchy,
+                                 "subRequerido" => $subRequerido,
+                                 "subtitleId" => $subtitleId,
+                                 "streamInfo" => $streamInfo,
+                                 "pass" => $pass,
+                                 "anime_title" => $anime_title,
+                                 "anime_id" => $anime_id
+                               );
+         }//fin while
+
+
+      respuesta_ok( array( "items" => $responseData, "count" => count($responseData) ), 200);
+    }//fin de if pending
+
+
+
+
+
   if( is_numeric($_GET['value']) ){
     //se esta recibiendo un numero, se consultara
 
