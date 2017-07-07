696cb4620aeae2a141b3228ffa150dc29f94c387
Hector Castillo <hectordanielunsc@gmail.com>
2017-07-06 19:45:23 -0500

    Agregación de sentencias a actualizar

--- api/nodes/crunchyroll/methods/PATCH.php
+++ api/nodes/crunchyroll/methods/PATCH.php
@@ -13,10 +13,39 @@ if( !is_numeric($_GET['value']) ){
 //entrada de datos
 $input = json_decode(file_get_contents('php://input'));
 
-
+//es forzado que se mande un pass para verificar y uno para actualizar
+if( !is_numeric($input->update->pass) || !is_numeric($input->pass) ){
+  error("Requerido pass actual del nodo y un nuevo pass", 400);
+}
 
 //preparar valores a actualizar
-$query_set = " pass = $input->pass ";
+$query_set = " pass = $input->update->pass "; //se estable el nuevo valor de pass
+
+                               //actualizar subRequerido
+                               if( is_numeric($input->update->subRequerido) ){
+                                 $query_set += " , subRequerido = $input->update->subRequerido ";
+                               }
+
+                               //actualizar subtitleId
+                               if( is_numeric($input->update->subtitleId) ){
+                                 $query_set += " , subtitleId = $input->update->subtitleId ";
+                               }
+
+                               //actualizar streamInfo
+                               if($input->update->streamInfo){
+                                 $query_set += " , streamInfo = '$input->update->streamInfo' "; //tiene que ser base64
+                               }
+
+                               //actualizar idEpisodio
+                               if( is_numeric($input->update->idEpisodio) ){
+                                 $query_set += " , idEpisodio = '$input->update->idEpisodio' ";
+                               }
+
+                               //actualizar idCrunchy
+                               if( is_numeric($input->update->idCrunchy) ){
+                                 $query_set += " , idCrunchy = $input->update->idCrunchy ";
+                               }
+
 
 echo $query_set;exit();
 
