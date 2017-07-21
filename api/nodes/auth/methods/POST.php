a0940b655155d100203b2b817ab80f35b646ae21
Hector Castillo <hectordanielunsc@gmail.com>
2017-07-21 15:48:27 -0500

    alterada forma de salida del expire time

--- api/nodes/auth/methods/POST.php
+++ api/nodes/auth/methods/POST.php
@@ -5,7 +5,11 @@
 //entrada de datos
 $input = json_decode(file_get_contents('php://input'));
 
+//asignar valores
 $username = $input->user;
 $password = $input->password;
 
+
+if( isset($username) and isset($password) ){ //validar si son aceptables los valores
 createSession($username, $password);
+}
