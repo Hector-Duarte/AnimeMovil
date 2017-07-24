67d02c217ca3332915c30750bf39b59fc9258e4e
Hector Castillo <hectordanielunsc@gmail.com>
2017-07-24 09:45:16 -0500

    movida función createSession a POST del nodo auth. se finalizo la función checkSession

--- api/nodes/auth/methods/GET.php
+++ api/nodes/auth/methods/GET.php
@@ -2,35 +2,14 @@
 
 //validar session
 
-//asignar valores
-if($_COOKIE['session_id']){
-  $session_id = $_COOKIE['session_id']; //asignar de cookie
-}else if($_GET['session_id']){
-  $session_id = $_GET['session_id']; //asignar de parametro GET ?session_id=IDSESSION
-}else{
-    error('session_id necesaria.', 400); //no se han encontrado parametros validos.
-}
-
-if( isset($session_id) ){ //validar si son aceptables los valores
-checkSession($session_id, 'API');
+checkSession('API', false); //'API' es el tipo de callback y el false es que no es necesario que sea admin
 
       if(SESSION_STATUS){ //si la session es valida.
-
-        setcookie("session_id", session_id(), $_SESSION['session_expire'], "/"); //asignar cookie
+$user_id, $user_level, $ip, $token, $expire
 
         respuesta_ok( array(
-        "id" => session_id(),
-        "ip" => $_SESSION['session_ip'],
-        "expire" => date('m-d-Y H:i:s', $_SESSION['session_expire']),
-        "user_info" =>      array(
-                                  "id" => $_SESSION['user_id'],
-                                  "username" => $_SESSION['user_name'],
-                                  "level" => $_SESSION['user_level']
-                                 )
+        "ip" => $ip,
+        "expire" => $expire
                            ), 200);
 
       }
-
-}else{
-  error('session_id no valida.', 400);
-}
--- api/nodes/auth/methods/POST.php
+++ api/nodes/auth/methods/POST.php
@@ -1,6 +1,77 @@
 <?php
-
 //validar session
+
+
+
+//crear session
+function createSession($usuario, $password){
+/* Esta función solo se usara en la api por lo que no necesita diferentes tipos de callbaks */
+
+  //abrir sql
+  $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
+      $prep_stmt = "SELECT password, salt, level, user, id from usuarios WHERE user=? LIMIT 1;";
+      $stmt = $mysqli->prepare($prep_stmt);
+      $stmt->bind_param('s', $usuario);
+      $stmt->execute();
+      $stmt->store_result();
+
+
+  // Obtiene las variables del resultado.
+          $stmt->bind_result($db_password, $db_salt, $session_user_level, $session_user_name, $session_user_id);
+
+          //verificar si la consulta retorno usuario
+          if( !$stmt->fetch() ){
+            error('No existe el usuario, verifica tu información.', 403); //el usuario no es existe
+          }
+
+
+
+
+
+
+  // Crea un hash con la contrasena y el salt.
+  $password = hash('sha512', $password . $db_salt);
+
+         // comprobar autenticacion de contraseñas
+              if ( $password!=$db_password ) {
+
+                       //contrasena incorrecta
+                       error('Verifica tu contraseña.', 403);
+
+              }else{
+
+         //destruir passwords
+         unset($password);
+         unset($db_password);
+         unset($db_salt);
+
+         //procotolo para crear la sesion
+         $token_expire = time() + 1296000; //expira en 15 dias
+         $token_access = generateHash($session_user_id.$session_user_name.$session_user_level.$token_expire); //token hash de acceso para la sesión --- hash_primary
+
+         //insertar datos en la tabla de sessiones
+         $prep_stmt = "INSERT INTO sessions(user_id, user_level, ip, token, expire) VALUES (?,?,?,?,?);";
+         $stmt = $mysqli->prepare($prep_stmt);
+         $stmt->bind_param('iissi', $session_user_id, $session_user_level, getUserIp(), $token_access, $token_expire);
+         $stmt->execute();
+
+         $session_id = $stmt->insert_id; //ID de la session
+
+         $hash_check = generateHash($session_id.$token_access); //hash_secundary
+
+
+         respuesta_ok( array( "auth" => "$session_id:$token_access:$hash_check", "expire" => $token_expire, "expire_in" => 1296000  ) , 201); //retornar la id generada y terminar function
+
+       } //fin de else
+
+       /* cerrar conexion */
+       $mysqli->close();
+} //fin de createSession
+
+
+
+
 
 //entrada de datos
 $input = json_decode(file_get_contents('php://input'));
