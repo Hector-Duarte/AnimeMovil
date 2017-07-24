d778228c00aa2fd325dec24326e8929875a0fd28
Hector Castillo <hectordanielunsc@gmail.com>
2017-07-24 09:47:07 -0500

    eliminación de variables no necesarias (mal ubicadas)

--- api/nodes/auth/methods/GET.php
+++ api/nodes/auth/methods/GET.php
@@ -5,7 +5,7 @@
 checkSession('API', false); //'API' es el tipo de callback y el false es que no es necesario que sea admin
 
       if(SESSION_STATUS){ //si la session es valida.
-$user_id, $user_level, $ip, $token, $expire
+
 
         respuesta_ok( array(
         "ip" => $ip,
