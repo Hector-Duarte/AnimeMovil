1a616cc0874b1c0c53cb54e5d68b8eca6c1eb10a
Hector Castillo <hectordanielunsc@gmail.com>
2017-07-26 14:55:09 -0500

    Actualizado valores para funcionar con las nuevas sessiones.

--- api/nodes/favoritos/methods/DELETE.php
+++ api/nodes/favoritos/methods/DELETE.php
@@ -4,18 +4,25 @@
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 
 
-    $prep_stmt = "DELETE FROM favoritos WHERE id = ? AND nodeId = ?  LIMIT 5;";
+    $prep_stmt = "DELETE FROM favoritos WHERE id = ? AND nodeId = ?  LIMIT 1;";
     $stmt = $mysqli->prepare($prep_stmt);
 
-    $stmt->bind_param('ii', $_COOKIE["session_user_id"], $_GET["value"]);
+    $user_id = USER_ID;//la ID del usuario.
+    $stmt->bind_param('ii', $user_id, $_GET["value"]);
 
+    $stmt->execute(); //ejecutar borrado
 
-if( $stmt->execute() ){
+    $del_exitoso = $stmt->affected_rows; //obtiene el numero de filas borradas (tiene que ser 1 que es true)
 
-   respuesta_ok( array( "available" => false ), 200);
+              $stmt->close(); //cerrar sentencia
+              $mysqli->close(); //cerrar sql
+
+if( $del_exitoso ){ //si se afecto una fila es que se borro (1 es true)
+
+   respuesta_ok( array( "available" => false, "message" => "Borrado con exito." ), 200);
 }else{
 
-   respuesta_ok( array( "available" => true ), 200);
+   respuesta_ok( array( "available" => true, "message" => "No se podido borrar el anime." ), 202);
 }
 
 
