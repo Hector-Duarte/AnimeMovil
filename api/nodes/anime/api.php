d7d43449522205e25c2079de6fe5233a7687ae9a
Hector Castillo <hectordanielunsc@gmail.com>
2017-07-26 21:37:14 -0500

    creado nodo "anime" para administrar los animes en la BD.

---
+++ api/nodes/anime/api.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case "GET":
+                 require_once("methods/GET.php");
+
+        break;
+    case "POST":
+                 require_once("methods/POST.php");
+
+        break;
+    case "DELETE":
+                 require_once("methods/DELETE.php");
+
+        break;
+        case "PATCH":
+                     require_once("methods/PATCH.php");
+
+            break;
+      default:
+                 error("Metodo no soportado", 405);
+
+}
