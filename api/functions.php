6f0b8e7845ae8ab3315a45aa4d68a59f9af3c3ab
Hector Castillo <hectordanielunsc@gmail.com>
2017-07-31 13:35:32 -0500

    test

--- api/functions.php
+++ api/functions.php
@@ -132,7 +132,8 @@ $_blobUrl = getBlobUrl($account_name,$container_name,$blob_name,'b','w',$end_dat
 
 
 //size de la imagen
-header('content-type: image/jpg');exit();
+$imagen_size = mb_strlen($lienzo);
+echo $imagen_size;exit();
 //cargar imagen a azure
 //cache al cdn de un año y cache al usuario de 7 dias
 $opts = array(
@@ -141,7 +142,7 @@ $opts = array(
     'header'=>"x-ms-blob-type:BlockBlob\r\n".
               "Content-Type:image/jpg\r\n".
               "x-ms-blob-cache-control:s-maxage=31536000, max-age=604800\r\n".
-              "Content-Length:\r\n",
+              "Content-Length:$imagen_size\r\n",
 'content' => $imagen_data
    )
 );
