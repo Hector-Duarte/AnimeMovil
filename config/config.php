5b0c81f0ddebd9cc030747c95e7b4b5377dac475
Hector Castillo <hectordanielunsc@gmail.com>
2017-08-06 03:41:11 -0500

    prueba de conexión a base de datos remota.

--- config/config.php
+++ config/config.php
@@ -10,9 +10,9 @@ define('DOMAIN', 'animemovil.com');
 define('STATIC_DOMAIN', 'media.animemovil.com');
 
 //base de datos
-define("HOST", "localhost");     // El alojamiento al que deseas conectarte
-define("USER", "root");    // El nombre de usuario de la base de datos
-define("PASSWORD", "7445018937");    // La contraseña de la base de datos
+define("HOST", "animemovil.mysql.database.azure.com");     // El alojamiento al que deseas conectarte
+define("USER", "jarri1999@animemovil");    // El nombre de usuario de la base de datos
+define("PASSWORD", "HUAWEIu8185");    // La contraseña de la base de datos
 define("DATABASE", "animemovil");    // El nombre de la base de datos
 
 //claves de acceso
